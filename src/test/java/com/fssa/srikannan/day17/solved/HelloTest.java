dcf9360c4aaf6de3ee1327b29e06cbf01f4b7d01
sri <116251899+sri-inovator@users.noreply.github.com>
2023-06-23 10:46:55 +0530

    Add files via upload

---
+++ src/test/java/com/fssa/srikannan/day17/solved/HelloTest.java
@@ -0,0 +1,22 @@
+package com.fssa.srikannan.day17.solved;
+
+import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Sample test class
+ * @author BharathwajSoundarara
+ *
+ */
+public class HelloTest {
+	
+	/**
+	 * First test case
+	 */
+	@Test
+	public void helloTest() {
+		
+		Assertions.assertEquals("Hello World", "Hello World");
+	}
+}
