e1a10735e5e7400bf41f961151d0bcb48c956416
sri <116251899+sri-inovator@users.noreply.github.com>
2023-06-26 23:53:35 +0530

    Add files via upload

---
+++ src/main/java/com/fssa/srikannan/day18/solved/ValidationUtil.java
@@ -0,0 +1,13 @@
+package com.fssa.srikannan.day18.solved;
+
+public class ValidationUtil{
+	public static boolean validateAge(int age) throws IllegalArgumentException {
+		if (age <= 0) {// Check if the age is a positive value
+			throw new IllegalArgumentException("Invalid age");
+
+		}  
+		else {
+			return true;
+		}
+	}
+}
---
+++ src/test/java/com/fssa/srikannan/day18/solved/TestValidation.java
@@ -0,0 +1,25 @@
+package com.fssa.srikannan.day18.solved;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestValidation {
+	
+	@Test // Annotation is marker for JUNIT to run this method automatically
+	public void testPostiveValidateAge() {
+		Assertions.assertTrue(ValidationUtil.validateAge(9));
+	}
+	
+	@Test
+	public void testNegativeValidateAge() {
+		try {
+			ValidationUtil.validateAge(-9);
+			Assertions.fail("ValidateAge failed");
+		}
+		catch(IllegalArgumentException ex) {
+			Assertions.assertEquals("Invalid age", ex.getMessage());
+		}
+	}
+	
+
+}
